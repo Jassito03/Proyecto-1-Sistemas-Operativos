61f5ae4b4a4f042812937902872bcab80450ae15
Jassito03 <joseph.pinar.baltodano@est.una.ac.cr>
2024-04-05 12:33:41 -0600

    cpu_t v0.1

--- cpu/cpu_t.c
+++ cpu/cpu_t.c
@@ -1,30 +1,5 @@
 #include "./cpu_t.h"
 
-typedef struct cpu_t {
-  // Identifier
-  char name[5];
-  // Processes executing in user mode
-  int cant_user_process;
-  // Niced processes executing in user mode
-  int cant_niced_process;
-  // Processes executing in kernel mode
-  int cant_system_process;
-  // Twiddling thumbs
-  int cant_idle;
-  // Waiting for I/O to complete
-  int cant_iowait;
-  // Servicing interrupts
-  int cant_irq;
-  // Servicing softirqs
-  int cant_softirq;
-  // Counts the ticks spent executing other virtual hosts
-  int cant_steal;
-  // Counts the time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
-  int cant_guest;
-  // Time spent running a niced guest
-  int cant_guest_nice;
-} cpu_t;
-
 void cpu_t_init(cpu_t *cpu_t, char new_name[5], int news_args[10]) {
   cpu_t->name[5] = new_name[5];
   cpu_t->cant_user_process = news_args[1];
--- cpu/cpu_t.h
+++ cpu/cpu_t.h
@@ -1,4 +1,27 @@
 #include <stdio.h>
 
-typedef struct cpu_t;
+typedef struct {
+  // Identifier
+  char name[5];
+  // Processes executing in user mode
+  int cant_user_process;
+  // Niced processes executing in user mode
+  int cant_niced_process;
+  // Processes executing in kernel mode
+  int cant_system_process;
+  // Twiddling thumbs
+  int cant_idle;
+  // Waiting for I/O to complete
+  int cant_iowait;
+  // Servicing interrupts
+  int cant_irq;
+  // Servicing softirqs
+  int cant_softirq;
+  // Counts the ticks spent executing other virtual hosts
+  int cant_steal;
+  // Counts the time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+  int cant_guest;
+  // Time spent running a niced guest
+  int cant_guest_nice;
+} cpu_t;
 void cpu_t_init(cpu_t *, char[5], int[10]);
