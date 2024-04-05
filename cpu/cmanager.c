09a9f884c0f7d6515b48049ae0f0a08cbcae6b46
Brandon0704 <79481328+Brandon0704@users.noreply.github.com>
2024-04-05 12:34:42 -0600

    mejora de char a int

--- cpu/cmanager.c
+++ cpu/cmanager.c
@@ -1,31 +1,32 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 
 #define MAX_BUFFER_SIZE 1024
 
-char* num_cores(const char* comando) {
-  char* cores = malloc(MAX_BUFFER_SIZE);
-    if(cores == NULL) {
-      perror("Error de asignación de memoria");
-      exit(EXIT_FAILURE);
-    }
+int cmanager_calc_cores(){ 
+    const char* comando = "cat /proc/cpuinfo | grep siblings | grep : | grep -m 1 2 | awk '{print $NF}'";
+    int cores;
 
     FILE* file = popen(comando, "r");
     if (file == NULL) {
       perror("Error al ejecutar el comando");
-      free(cores);
       exit(EXIT_FAILURE);
     }
   
-  fgets(cores,MAX_BUFFER_SIZE, file);
+  if(fscanf(file,"%d",&cores) != 1){
+    perror("Error al leer la salida del comando");
+    pclose(file);
+    exit(EXIT_FAILURE);
+  }
+  pclose(file);
 
   return cores;
 }
 
 int main() {
-    const char* comando = "cat /proc/cpuinfo | grep siblings | grep : | grep -m 1 2 | awk '{print $NF}'";
-    char* salida = num_cores(comando);
-    printf("Salida: %s", salida);
-    free(salida);
+    int salida = cmanager_calc_cores();
+    salida = salida + 1;
+    printf("Salida: %d", salida);
     return 0;
 }
